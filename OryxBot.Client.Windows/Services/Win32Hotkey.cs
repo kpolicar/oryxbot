3c6253b260d5553d428ae740df46222887250414
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-02-20 11:42:26 +0100

    remove f3 test move mouse

--- OryxBot.Client.Windows/Services/Win32Hotkey.cs
+++ OryxBot.Client.Windows/Services/Win32Hotkey.cs
@@ -21,17 +21,10 @@ namespace OryxBot.Client.Windows.Services
             var combinations = new Dictionary<Combination, Action> {
                 {Combination.FromString("F1"), () => F1?.Invoke(this, EventArgs.Empty)},
                 {Combination.FromString("F2"), () => F2?.Invoke(this, EventArgs.Empty)},
-                {Combination.FromString("F3"), () => Testmove()},
                 {Combination.FromString("Escape"), () => Escape?.Invoke(this, EventArgs.Empty)}
             };
             
             m_GlobalHook.OnCombination(combinations);
-        }
-
-        private void Testmove() {
-            var inp = Program.Services.GetService<Input>();
-            var dir =Vector2.Normalize(new Vector2(rand.Next(-1000,1000), rand.Next(-1000,1000)));
-            inp.MoveCursorRelativeToCenter(dir);
         }
 
         public void Dispose() {
