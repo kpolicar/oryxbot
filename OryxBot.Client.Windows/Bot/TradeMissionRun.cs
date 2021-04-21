298b4e53908a3d8fd9495cd78e230f95d4fa8038
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-04-21 13:29:51 +0200

    first step on trade route run is run to bank

--- OryxBot.Client.Windows/Bot/TradeMissionRun.cs
+++ OryxBot.Client.Windows/Bot/TradeMissionRun.cs
@@ -71,7 +71,7 @@ namespace OryxBot.Client.Windows.Bot
         }
 
         private void ResetRun() {
-            _step = new RunRouteToDestination(Route);
+            _step = new RunToBank();
             Reset?.Invoke(this, EventArgs.Empty);
         }
 
