054a9aa63bcd7b51c87095e5c780002e573a1883
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-04-22 19:52:51 +0200

    v0.5beta version endpoint and name

--- OryxBot.Client.Windows/Program.cs
+++ OryxBot.Client.Windows/Program.cs
@@ -26,9 +26,9 @@ namespace OryxBot.Client.Windows
         #endif
         
         public static byte[] AppKey => System.Convert.FromBase64String(_appKey);
-        public const string VersionNumber = "4";
-        public const string Version = "v0.4 Beta";
-        public const string VersionEndpoint = "v0.4beta";
+        public const string VersionNumber = "5";
+        public const string Version = "v0.5 Beta";
+        public const string VersionEndpoint = "v0.5beta";
         
         public static readonly Kernel _kernel = new();
         public static ServiceContainer Services => _kernel.Services;
